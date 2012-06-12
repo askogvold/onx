a98b3b0ab3b230aea9799763e989ce5017aeaa33
Arve Skogvold <arve@skogvold.org>
2012-06-13 00:32:03 +0300

    Formatting

--- workHours.js
+++ workHours.js
@@ -1,5 +1,5 @@
 /*
- *	Script to track my work hours automatically.
+ *  Script to track my work hours automatically.
  *  Set up a 500m radius border around my workplace, and measuring the time spent within the area.
  *
  *  Testing on my way to work tomorrow :-)
@@ -8,54 +8,55 @@
 
 var myEmail = "mail@example.com";
 
-var workplace = device.regions.createRegion(
-    {name:"Rotvoll", 
-    latitude:  63.43915607186877, 
-    longitude: 10.480849756066846, 
-    radius:    500});
+var workplace = device.regions.createRegion({
+	name:"Rotvoll", 
+	latitude:  63.43915607186877, 
+	longitude: 10.480849756066846, 
+	radius:    500
+});
 
 var dailyWorkplaceCounter = 0;
 
 var workplaceEntry = new Date(0);
 
-workplace.on("enter", function(signal){
-    dailyWorkplaceCounter += 1;
-    workplaceEntry = new Date();
-    var notification = device.notifications.createNotification("Entering workplace for the " + dailyWorkplaceCounter + ". time today. Enjoy!");    
-    notification.show();
+workplace.on("enter", function(signal) {
+	dailyWorkplaceCounter += 1;
+	workplaceEntry = new Date();
+	var notification = device.notifications.createNotification("Entering workplace for the " + dailyWorkplaceCounter + ". time today. Enjoy!");
+	notification.show();
 });
 
-workplace.on("exit", function(signal){
-    var now = new Date();
-    var remainingSeconds = (now.getTime() - workplaceEntry.getTime())/1000;
-    var hours = remainingSeconds / (60*60);
-    remainingSeconds %= 60*60;
-    var minutes = remainingSeconds / 60;
-    remainingSeconds %= 60;
-    var seconds = remainingSeconds;
-    
-    var durationString = "" + hours + ":" + minutes + ":" + seconds;
-    
-    var notification = device.notifications.createNotification("Left work, you stayed for " + durationString);
-    notification.show();
-    
-    // And send mail
-    device.messaging.sendMail(
-        {
-            to: myEmail,
+workplace.on("exit", function(signal) {
+	var now = new Date();
+	var remainingSeconds = (now.getTime() - workplaceEntry.getTime())/1000;
+	var hours = remainingSeconds / (60*60);
+	remainingSeconds %= 60*60;
+	var minutes = remainingSeconds / 60;
+	remainingSeconds %= 60;
+	var seconds = remainingSeconds;
+
+	var durationString = "" + hours + ":" + minutes + ":" + seconds;
+
+	var notification = device.notifications.createNotification("Left work, you stayed for " + durationString);
+	notification.show();
+
+	// And send mail
+	device.messaging.sendMail(
+		{
+			to: myEmail,
 			subject: 'AUTO:WORKHOURS',
 			body: "Arrived work at " + workplaceEntry.toString() + ".\nLeft work at " + now.toString() + ".\n\nTime spent: " + durationString
-        },
-        function (err) {
-            console.log(err || 'mail was sent successfully');
-        }
-    );
+		},
+		function (err) {
+			console.log(err || 'mail was sent successfully');
+		}
+	);
 });
 
 var now = new Date();
 var timerStart = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
 var timerAction = function() {
-    workplaceEntry = 0;
+	workplaceEntry = 0;
 };
 
 device.scheduler.setTimer(
